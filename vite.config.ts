7f2e8fd0a6682c352271481d258728dd16700c8b
BuiHenry1404 <buihenry1404@gmail.com>
2025-06-14 11:26:03 +0700

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personal_project_front-end/",
   plugins: [react()],
   define: {
     "process.env": {},
